e5096b7d31c257d54b2eba3cdeda3d4a077e8794
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-06 12:47:00 +0330

    js dom

---
+++ script.js
@@ -0,0 +1,5 @@
+const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+
