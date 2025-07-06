d5f8ff6d00070acdf782f2c36b1629384d6c3a11
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-06 12:59:29 +0330

    js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const optionMenu = document.querySelector(".select-menu"),
     options = optionMenu.querySelectorAll(".option"),
     sBtn_text = optionMenu.querySelector(".sBtn-text");
 
+
+selectBtn.addEventListener("click",()=>{
+    optionMenu.classList.toggle("active");
+})
+
+options.forEach(option => {
+  option.addEventListener("click",()=>{
+    let selectedOption=option.querySelector(".option-text").innerText;
+    sBtn_text.innerText=selectedOption;
+
+    optionMenu.classList.remove('active');
+  })
+})
